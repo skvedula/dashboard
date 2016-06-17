54b66be34c4ee7372f35efb658cb98d11b8736eb
skvedula <skvedula@outlook.com>
2016-06-17 17:00:47 +0530

    removing redundant code
    removing redundant code

--- js/backup/module.js
+++
@@ -1,1 +0,0 @@
-angular.module('Dash', ['ui.bootstrap', 'ui.router', 'ngCookies']);
--- js/morris/morris.js
+++
@@ -1,8 +0,0 @@
-Morris.Donut({
-  element: 'chart',
-  data: [
-    {label: "Credit", value: 12},
-    {label: "Debit", value: 30},
-    {label: "Loan", value: 20}
-  ]
-});
