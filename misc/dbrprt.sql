8487eedc873fe09d0a9b2ba759ae1bd25418a51b
skvedula <skvedula@outlook.com>
2016-06-17 15:20:16 +0530

    Removing node_modules and added table creation
    Removing node_modules and added table creation

---
+++ misc/dbrprt.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM sys.testdb
+LIMIT 0, 1000
+
+-- Date: 2016-06-14 22:03
+*/
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test1','pwn417','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test2','pwn417','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test3','pwn531','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test4','pwn333','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test5','pwn552','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test6','pwn492','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('testt7','pwn379','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test8','pwn767','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test9','pwn169','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test10','pwn314','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
+INSERT INTO `testdb` (`db_name`,`server`,`db_file`) VALUES ('test11','pwn324','L:Program FilesExchsrvrFXGEXEC001FXGEXEC001.edb');
