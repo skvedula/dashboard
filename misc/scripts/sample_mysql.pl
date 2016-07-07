93aa894e2b85ca68ceddd5bf6402b319dbfcd5b4
skvedula <skvedula@outlook.com>
2016-07-07 09:44:55 +0530

    Modified script emailReport to insert data into DB

--- misc/scripts/sample_mysql.pl
+++ misc/scripts/sample_mysql.pl
@@ -19,14 +19,16 @@ $database = "sys";
 $connection = ConnectToMySql($database);
 
 # set the value of your SQL query
-$query = "insert into address (name_first, name_last, address_01, address_02, address_city, address_state, address_postal_code) 
-			values (?, ?, ?, ?, ?, ?, ?) ";
+#$query = "insert into email_domains (domain_type, domain_name, domain_mapping_count, administered_by) 
+			#values (?, ?, ?, ?) ";
+$query = "insert into email_domains (domain_type, domain_mapping_count) 
+values (?, ?) ";
 
 # prepare your statement for connecting to the database
 $statement = $connection->prepare($query);
 
 # execute your SQL statement
-$statement->execute('John', 'Smith', '100 Main Street', 'Suite 500', 'Buffalo', 'NY', '14201');
+$statement->execute('Microsoft Exchange', 23);
 
 #----------------------------------------------------------------------
 
@@ -36,27 +38,24 @@ $statement->execute('John', 'Smith', '100 Main Street', 'Suite 500', 'Buffalo', 
 #----------------------------------------------------------------------
 
 # set the value of your SQL query
-$query2 = "select name_first, name_last, address_01, address_02, address_city, address_state, address_postal_code from address where name_last = 'Smith'";
+$query2 = "select * from email_domains";
 
 # prepare your statement for connecting to the database
 $statement = $connection->prepare($query2);
 
 # execute your SQL statement
 $statement->execute();
-
 # we will loop through the returned results that are in the @data array
 # even though, for this example, we will only be returning one row of data
 
    while (@data = $statement->fetchrow_array()) {
-      $name_first = $data[0];
-      $name_last = $data[1];
-      $address_01 = $data[2];
-      $address_02 = $data[3];
-      $address_city = $data[4];
-      $address_state = $data[5];
-      $address_postal_code = $data[6];
-
-print "RESULTS - $name_first, $name_last, $address_01, $address_02, $address_city, $address_state, $address_postal_code\n";
+      $d_type = $data[0];
+     # $d_name = $data[1];
+      $d_map_count = $data[1];
+      #$admin_by = $data[3];
+      
+#print "RESULTS - $d_type, $d_name, $d_map_count, $admin_by\n";
+print "RESULTS - $d_type, $d_map_count\n";
 
 }
 
@@ -72,20 +71,14 @@ sub ConnectToMySql {
 
 my ($db) = @_;
 
-# open the accessDB file to retrieve the database name, host name, user name and password
-open(ACCESS_INFO, "<..\/accessAdd") || die "Can't access login credentials";
-
 # assign the values in the accessDB file to the variables
-my $database = <ACCESS_INFO>;
-my $host = <ACCESS_INFO>;
-my $userid = <ACCESS_INFO>;
-my $passwd = <ACCESS_INFO>;
+my $database = 'sys';
+my $host = 'localhost';
+my $userid = 'pinuser';
+my $passwd = 'pin@123';
 
 # assign the values to your connection variable
-my $connectionInfo="dbi:mysql:$db;$host";
-
-# close the accessDB file
-close(ACCESS_INFO);
+my $connectionInfo="dbi:mysql:sys;localhost";
 
 # the chomp() function will remove any newline character from the end of a string
 chomp ($database, $host, $userid, $passwd);
