f92e0c904f16b9e259fd83a1210b7e8397872132
triven312 <triven312@gmail.com>
2016-11-08 08:49:50 +0530

    Changes related to parser

--- misc/parser.js
+++ misc/parser.js
@@ -6,43 +6,24 @@ var body = "";
 function callb(error, stdout, stderr) {
 	data = fs.readFileSync('output').toString();
 	data = data.split("\n");
-	// main_header_1 = data[0];
-	// body = body + "<h2>" + main_header_1 + "</h2>";
-	// first table
-	// table_heading_1 = data[2];
-	// body = body + "<h3>" + table_heading_1 + "</h3>";
 	timestamp_1 = data[0];
 	body = body + "<h5>" + timestamp_1 + "</h5>";
 	// console.log(body);
 	table_head_1 = "<table><thead><tr><th>Channel</th><th>Messages</th><th>Size (Mb)</th></tr></thead>";
 	body = body + table_head_1;
-	tdata_1 = data.slice(6,20);//table 1 data tr<->td elements
+	tdata_1 = data.slice(3,11);//table 1 data tr<->td elements
 	tdata_1 = tdata_1.map(function(s) { return s.trim() });
-	tdata_1.pop();
-	tndata_1 = tdata_1.join("\n\r");
-	data_arr_1 = tdata_1.join("\n\r");
-	data_arr_1 = data_arr_1.replace(/[\n\r]/g, "#");
-	data_arr_1 = data_arr_1.trim();
-	data_arr_1 = data_arr_1.split("####");
-	data_arr_1 = data_arr_1.map(function(s) { return s.replace(/\s\s+/g, ' ') });
-	tresult_1 = data[24];
+	data_arr_1 = tdata_1.map(function(s) { return s.replace(/\s\s+/g, ' ') });
+	// console.log(data_arr_1);
+	tresult_1 = data[12];
+	// console.log(tresult_1);
 	for(var i=0; i<data_arr_1.length; i++) {
 		td_arr_1 = data_arr_1[i].split(" ");
 		body = body + "<tr>" + "<td>" + td_arr_1[0] + "</td>" + "<td>" + td_arr_1[1] + "</td>" + "<td>" + td_arr_1[2] + "</td>" + "</tr>";
-		// td_arr_2 = data_arr_2[i].split(" ");
-		// console.log(td_arr_1);
 	}
 	tsummary_1 = tresult_1.trim().replace(/\s\s+/g, ' ').split(" ");
 	body = body + "<tfoot>" + "<tr>" + "<td>" + tsummary_1[0] + "</td>" + "<td>" + tsummary_1[1] + "</td>" + "<td>" + tsummary_1[2] + "</td>" + "</tr>" + "</tfoot>";
-	// tsummary_2 = tresult_2.trim().replace(/\s\s+/g, ' ').split(" ");
-	// console.log(tsummary_1);
-	// console.log(tsummary_2);
-// [5:23] table 1
-// [5:23] table 1
-
-body = body + '</table>';
-// console.log(body);
-// console.log("\n\n");
+	body = body + '</table>';
 }
 exec("cat sample1.txt > output", callb);// **change this commad to ssh perl command
 exec("cat sample2.txt > output", callb);// **change this commad to ssh perl command
