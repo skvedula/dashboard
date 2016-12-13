328a938baa839b74164e88a81ec13b568d66332a
triven312 <triven312@gmail.com>
2016-12-13 22:25:00 +0530

    Modifications to bash script with server names

--- misc/bash.sh
+++ misc/bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+echo 'Getting info from server 22025'
 ssh root@prh22025.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo '\n'
+echo 'Getting info from server 22026'
 ssh root@prh22026.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo '\n'
+echo 'Getting info from server 22027'
 ssh root@prh22027.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo '\n'
+echo 'Getting info from server 22028'
 ssh root@prh22028.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo '\n'
