97f582f28a9c6660e090ab4d458255c88dd7deec
triven312 <triven312@gmail.com>
2016-12-16 19:19:17 +0530

    Modified bash script

--- misc/bash.sh
+++ misc/bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo 'Getting info from server 22025'
+echo 'Total requests from 22025 server :'
 ssh root@prh22025.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo 'Getting info from server 22026'
+echo 'Total requests from 22026 server :'
 ssh root@prh22026.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo 'Getting info from server 22027'
+echo 'Total requests from 22027 server :'
 ssh root@prh22027.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
-echo 'Getting info from server 22028'
+echo 'Total requests from 22028 server :'
 ssh root@prh22028.prod.fedex.com "/usr/bin/mailq | grep 'Total requests:'"
